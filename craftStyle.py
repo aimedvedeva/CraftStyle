dd524d0a9fb7ea9812f1746e5040076a1c55eaa0
alesanm <alesan.med@yandex.ru>
2023-05-14 01:11:01 +0200

    remove comments

--- craftStyle.py
+++ craftStyle.py
@@ -19,25 +19,6 @@ def createCustomerPlanTable():
                                                                     expired boolean);")
     cur.execute("commit")
 
-# def createCustomerSessionTable():
-#     cur = connect()
-#     cur.execute(
-#         "CREATE table if not EXISTS CraftStyle.CustomerSession(customerSessionId INT primary key GENERATED ALWAYS AS IDENTITY,\
-#         customerId INT REFERENCES CraftStyle.Customer(customerId),\
-#         recommendation text,\
-#         pucturesNumber INT,\
-#         tags varchar,\
-#         sessionDate date);")
-#     cur.execute("commit")
-
-
-# def createPictureSessionTable():
-#     cur = connect()
-#     cur.execute(
-#         "CREATE table if not EXISTS CraftStyle.SessionPicture(sessionId INT REFERENCES CraftStyle.CustomerSession(customerSessionId),\
-#         pictureId INT REFERENCES CraftStyle.Picture(pictureId));")
-#     cur.execute("COMMIT")
-
 def purchaseSubscription(customer_id, subscription_plan):
     cur = connect()
     cur.execute("set transaction isolation level serializable;")
@@ -132,44 +113,3 @@ def processCustomerSession(customer_id, tags, picture_urls):
     session_id = createCustomerSession(customer_id, tags, picture_urls)
     recommendation = getRecommendation(picture_urls, tags)
     updateSessionRecommendation(session_id, recommendation)
-
-
-
-
-# def createCustomerSession(customer_id, pictures, picure_links, tags):
-#     cur = connect()
-#     cur.execute("set transaction isolation level serializable;")
-#     cur.execute("begin;")
-#
-#     try:
-#         # get allowed_sessions of a customer's subscription plan
-#         cur.execute("""select s.allowedsessions \
-#                        from subscriptionplan s \
-#                        join customerplan c on s.planid = c.subscriptionplanid where customerID = %s""", (customer_id))
-#         allowed_sessions = cur.fetchone()[0]
-#
-#         # get number of sessions that the customer has already had
-#         cur.execute("""select c.sessionsnumber from customer c where c.customerid = %s;""", (customer_id))
-#         done_sessions = cur.fetchone()[0]
-#
-#         # check if the customer doesn't exceed the limit according to his subscription plan
-#         if done_sessions >= allowed_sessions:
-#             raise ValueError("Unfortunately, all you free sessions have already used. You can upgrade you subscription plan.")
-#
-#         update_sessions_number_query = """UPDATE customer SET sessionsnumber = sessionsnumber + 1 WHERE custometID = %s;"""
-#
-#         cur.execute(update_sessions_number_query, (customer_id))
-#
-#         # save data about the session
-#         cur.execute("""INSERT INTO CustomerSession(customerID, Recommendation, pucturesNumber, tags, sessionDate)
-#         VALUES(%s, NULL, %s, %s, current_date);""", (customer_id, len(pictures), tags))
-#
-#         # save pictures
-#         for picture, link in zip(pictures, picure_links):
-#             cur.execute("""INSERT INTO Picture(customerID, pictureUrl, tags) VALUES(%s, %s, %s);""",\
-#                         (customer_id, link, tags))
-#         cur.execute("COMMIT")
-#
-#     except Exception as e:
-#         # raise an exception if any error occur
-#         cur.execute("Rollback;")
