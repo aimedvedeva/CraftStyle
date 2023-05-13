9c4fd69181cf650648570884de98f954b1b81842
alesanm <alesan.med@yandex.ru>
2023-05-14 01:02:14 +0200

    add recommendation module

---
+++ recommendation.py
@@ -0,0 +1,34 @@
+import openai
+
+openai.api_key = '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'
+
+def getRecommendation(image_urls, style_tags):
+    prompt = "Given the following images and style tags, please provide a textual recommendation on how to finalize your look:\n"
+
+    # Append the image URLs and style tags to the prompt
+    for image_url, style_tag in zip(image_urls, style_tags):
+        prompt += f"![Image]({image_url})\nStyle tag: {style_tag}\n"
+
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    recommendation = response.choices[0].text.strip()
+
+    return recommendation
+
+# Example usage
+image_urls = [
+    'https://drive.google.com/your-image-url1.jpg',
+    'https://drive.google.com/your-image-url2.jpg',
+    'https://drive.google.com/your-image-url3.jpg'
+]
+style_tags = ['casual', 'formal', 'sporty']
+
+recommendation = get_textual_recommendation(image_urls, style_tags)
+print(recommendation)
