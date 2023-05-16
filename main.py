9d84c64b3d7485a2cfb21a9eaae1eb26be75c808
MawadaAlabri <mooodi1396@gmail.com>
2023-05-16 10:42:24 +0400

    while loop

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from populate import populate
-
+from session import *
 if __name__ == '__main__':
+    x=0
     populate()
+    while x<10000:
+         createSession(11,'rget','office')
+         deleteCustomerSessions(11)
+         x+=1
+    
+
+    
+   
