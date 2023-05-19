11fdecd04ea9a728ff0a6aa0073eb96e2139abca
alesanm <alesan.med@yandex.ru>
2023-05-19 18:08:55 +0200

    add method for generating picture_id

--- picture.py
+++ picture.py
@@ -1,14 +1,21 @@
+import uuid
+from datetime import date
+
 from connect_redis import connect_redis
 
+def _generate_picture_id():
+    session_id = str(uuid.uuid4())
+    return session_id
 
-def add_picture(picture_id, upload_date, customer_id, tags, picture_link):
+def add_picture(customer_id, tags, picture_link):
+    picture_id = _generate_picture_id()
     redis_client = connect_redis()
 
     # Create a hash key for the picture
     picture_key = f'craft_style_picture:{picture_id}'
 
     # Add the picture data as hash fields
-    redis_client.hset(picture_key, 'upload_date', upload_date)
+    redis_client.hset(picture_key, 'upload_date', str(date.today()))
     redis_client.hset(picture_key, 'customer_id', customer_id)
     redis_client.hset(picture_key, 'tags', tags)
     redis_client.hset(picture_key, 'picture_link', picture_link)
