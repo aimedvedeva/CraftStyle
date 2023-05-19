0e590c6d0144770b58edd15a42a2600e6e8f2144
alesanm <alesan.med@yandex.ru>
2023-05-19 18:12:02 +0200

    modify HRPS test adding new functions to populate redis with customer sessions

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from populate import populate_postgre
+from populate import populate_redis_with_sessions
 
 if __name__ == '__main__':
-    populate_postgre()
+    # create_scheme()
+    # create_tables()
+    # populate_postgre_tables()
+    # populate_subscriptions()
 
     # 1. Smart cache. Keep copies of entities from PostgreSQL database in Redis + calculate / check counters.
     # (like an active sessions count).
 
-    # sessions_number = 0
-    # while sessions_number < 10000:
-    #      createSession(11, 'rget' , 'office')
-    #      deleteCustomerSessions(11)
-    #      sessions_number += 1
+    sessions_number = 0
+    while sessions_number < 10:
+        populate_redis_with_sessions()
+        sessions_number += 1
